55335f7e4396f3f09eecd5a8e081d302f2766f78
Nasim <nasimhelal123>
2023-03-06 19:02:33 +0600

    Logout functionality included

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <div className="navbar container mx-auto flex items-center justify-between border-b border-sky-900">
       <Link to="/" className="logo py-5 text-sky-400 text-2xl font-medium">
         Proxima
       </Link>
       <nav className="flex gap-5 items-center">
-        <Link to="/login" className="hover:text-sky-400 duration-300">
-          Login
-        </Link>
-        <Link to="/signup" className="hover:text-sky-400 duration-300">
-          SignUp
-        </Link>
+        {!user && (
+          <div className="flex gap-5 items-center">
+            <Link to="/login" className="hover:text-sky-400 duration-300">
+              Login
+            </Link>
+            <Link to="/signup" className="hover:text-sky-400 duration-300">
+              SignUp
+            </Link>
+          </div>
+        )}
+
+        {user && (
+          <div className="flex gap-5 items-center">
+            <span>{user.email}</span>
+            <button
+              onClick={handleLogout}
+              type="submit"
+              className="bg-rose-500 text-white px-7 py-3 rounded-lg hover:bg-sky-50 hover:text-slate-900 duration-300"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </nav>
     </div>
   );
