f039dbf1facaecfc525d9fd3f10bd985f75e763f
Nasim <nasimhelal123>
2023-05-27 09:46:16 +0600

    Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="py-10 border-t-2 border-sky-500 text-center text-xl">
+      &copy;{new Date().getFullYear()} - All Rights Reserved.
+    </div>
+  );
 };
 
 export default Footer;
