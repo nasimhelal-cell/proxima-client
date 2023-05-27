3b773eb896a90b265ab0d0946476f927b6b702fa
Nasim <nasimhelal123>
2023-05-27 09:53:57 +0600

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="py-10 border-t-2 border-sky-500 text-center text-xl">
+    <div className="py-5 border-t-2 border-sky-500 text-center text-lg">
       &copy;{new Date().getFullYear()} - All Rights Reserved.
     </div>
   );
